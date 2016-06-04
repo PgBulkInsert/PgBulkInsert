cfefc58e35ce65ca14ade5945b3a44a4f80d7903
Philipp Wagner <bytefish@gmx.de>
2016-06-04 20:57:20 +0200

    PgBulkInsert Release 0.6

--- PgBulkInsert/deployment/create_bundle.bat
+++ PgBulkInsert/deployment/create_bundle.bat
@@ -16,7 +16,7 @@ set STDOUT=stdout.log
 set STDERR=stderr.log
 
 :: Version to build the bundle for:
-set VERSION=0.5
+set VERSION=0.6
 
 :: Set the Target Bundle file:
 set TARGET_BUNDLE=bundle\pgbulkinsert-bundle-%VERSION%.jar
