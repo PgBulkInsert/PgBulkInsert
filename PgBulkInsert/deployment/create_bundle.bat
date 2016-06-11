cac68f68f88a97ae0c5236404863d7093ed3c378
Philipp Wagner <bytefish@gmx.de>
2016-06-11 11:37:16 +0200

    Release 0.8
    Minor Bugfixes

--- PgBulkInsert/deployment/create_bundle.bat
+++ PgBulkInsert/deployment/create_bundle.bat
@@ -16,7 +16,7 @@ set STDOUT=stdout.log
 set STDERR=stderr.log
 
 :: Version to build the bundle for:
-set VERSION=0.7
+set VERSION=0.8
 
 :: Set the Target Bundle file:
 set TARGET_BUNDLE=bundle\pgbulkinsert-bundle-%VERSION%.jar
