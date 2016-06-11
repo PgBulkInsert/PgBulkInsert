2d6ea743502395448cba3b39b7e5d314cd892b7c
Philipp Wagner <bytefish@gmx.de>
2016-06-11 11:24:40 +0200

    FlushInterval Unit Test
    * Added a Unit Test to verify the Flush Interval

--- PgBulkInsert/src/test/de/bytefish/pgbulkinsert/pgsql/processor/BulkProcessorTest.java
+++ PgBulkInsert/src/test/de/bytefish/pgbulkinsert/pgsql/processor/BulkProcessorTest.java
@@ -15,6 +15,7 @@ import org.postgresql.PGConnection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +44,27 @@ public class BulkProcessorTest extends TransactionalTestBase {
         // The Processor should have fired 5 times, so 50 persons have been added:
 
         Assert.assertEquals(50, getRowCount());
+    }
+
+    @Test
+    public void testAddTimeBased() throws Exception {
+        // Create the BulkInserter to be wrapped:
+        PersonBulkInserter personBulkInserter = new PersonBulkInserter();
+        // Create the ConnectionFactory:
+        Func1<PGConnection> connectionFactory = () -> PostgreSqlUtils.getPGConnection(connection);
+        // Create the BulkProcessor:
+        BulkProcessor<Person> bulkProcessor = new BulkProcessor<>(personBulkInserter, connectionFactory, 10, Duration.ofSeconds(1));
+        // Create some Test data:
+        List<Person> fiftyPersons = getPersonList(3);
+        // Now process them with the BulkProcessor:
+        for (Person p : fiftyPersons) {
+            bulkProcessor.add(p);
+        }
+        // Sleep for 2 Seconds:
+        Thread.sleep(Duration.ofSeconds(2).toMillis());
+
+        // The three items should have been added:
+        Assert.assertEquals(3, getRowCount());
     }
 
     private List<Person> getPersonList(int numPersons) {
