48877d358bc3caecf951624dcd70bf84e73a4105
mariusz <mariusz@mariusz-mariusz>
2018-11-22 20:52:20 +0100

    Improved String to byte array conversion

--- PgBulkInsert/src/main/java/de/bytefish/pgbulkinsert/pgsql/handlers/StringValueHandler.java
+++ PgBulkInsert/src/main/java/de/bytefish/pgbulkinsert/pgsql/handlers/StringValueHandler.java
@@ -4,15 +4,14 @@
 package de.bytefish.pgbulkinsert.pgsql.handlers;
 
 import java.io.DataOutputStream;
-import java.nio.charset.Charset;
+
+import de.bytefish.pgbulkinsert.util.StringUtils;
 
 public class StringValueHandler extends BaseValueHandler<String> {
-	
-	private Charset utf8Charset = Charset.forName("UTF-8");
 
     @Override
     protected void internalHandle(DataOutputStream buffer, final String value) throws Exception {
-        byte[] utf8Bytes = value.getBytes(utf8Charset);
+        byte[] utf8Bytes = StringUtils.getUtf8Bytes(value);
 
         buffer.writeInt(utf8Bytes.length);
         buffer.write(utf8Bytes);
