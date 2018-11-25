6c49a89820a11f4c29ecf6079c66bd55b5619ed4
Mariusz Kowalczyk <mariusz.kowalczyk1@gmail.com>
2018-11-25 10:24:49 +0100

    Added overloaded saveAll method

--- PgBulkInsert/src/main/java/de/bytefish/pgbulkinsert/PgBulkInsert.java
+++ PgBulkInsert/src/main/java/de/bytefish/pgbulkinsert/PgBulkInsert.java
@@ -14,6 +14,7 @@ import org.postgresql.copy.CopyManager;
 import org.postgresql.copy.PGCopyOutputStream;
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -35,6 +36,13 @@ public class PgBulkInsert<TEntity> implements IPgBulkInsert<TEntity> {
         this.mapping = mapping;
     }
 
+    /**
+     * Save stream of entities
+     * 
+     * @param connection underlying db connection
+     * @param entities stream of entities
+     * @throws SQLException
+     */
     public void saveAll(PGConnection connection, Stream<TEntity> entities) throws SQLException {
 
         CopyManager cpManager = connection.getCopyAPI();
@@ -46,23 +54,49 @@ public class PgBulkInsert<TEntity> implements IPgBulkInsert<TEntity> {
             bw.open(new PGCopyOutputStream(copyIn, 1));
 
             // Insert Each Column:
-            entities.forEach(entity -> this.saveEntity(bw, entity));
+            entities.forEach(entity -> saveEntitySynchonized(bw, entity));
+        }
+    }
+    
+    /**
+     * Save collections of entities
+     * 
+     * @param connection underlying db connection
+     * @param entities collection of entities
+     * @throws SQLException
+     */
+    public void saveAll(PGConnection connection, Collection<TEntity> entities) throws SQLException {
+
+        CopyManager cpManager = connection.getCopyAPI();
+        CopyIn copyIn = cpManager.copyIn(mapping.getCopyCommand());
+
+        try (PgBinaryWriter bw = new PgBinaryWriter(configuration.getBufferSize())) {
+
+            // Wrap the CopyOutputStream in our own Writer:
+            bw.open(new PGCopyOutputStream(copyIn, 1));
+
+            // Insert Each Column:
+            entities.forEach(entity -> saveEntity(bw, entity));
         }
     }
 
     private void saveEntity(PgBinaryWriter bw, TEntity entity) throws SaveEntityFailedException {
-        synchronized (bw) {
-            // Start a New Row:
-            bw.startRow(mapping.getColumns().size());
+        // Start a New Row:
+        bw.startRow(mapping.getColumns().size());
 
-            // Iterate over each column mapping:
-            mapping.getColumns().forEach(column -> {
-                try {
-                    column.getWrite().accept(bw, entity);
-                } catch (Exception e) {
-                    throw new SaveEntityFailedException(e);
-                }
-            });
+        // Iterate over each column mapping:
+        mapping.getColumns().forEach(column -> {
+            try {
+                column.getWrite().accept(bw, entity);
+            } catch (Exception e) {
+                throw new SaveEntityFailedException(e);
+            }
+        });
+    }
+    
+    private void saveEntitySynchonized(PgBinaryWriter bw, TEntity entity) throws SaveEntityFailedException {
+        synchronized (bw) {
+        	saveEntity(bw, entity);
         }
     }
 }
