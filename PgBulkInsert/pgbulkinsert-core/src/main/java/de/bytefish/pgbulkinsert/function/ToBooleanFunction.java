ea4103aca6501ab697a4f171b6f09823465d28ea
Philipp Wagner <pwag86@googlemail.com>
2020-05-11 21:00:31 +0200

    Remove my name in copyrights. Set Version to 5.1.0

--- PgBulkInsert/pgbulkinsert-core/src/main/java/de/bytefish/pgbulkinsert/function/ToBooleanFunction.java
+++ PgBulkInsert/pgbulkinsert-core/src/main/java/de/bytefish/pgbulkinsert/function/ToBooleanFunction.java
@@ -1,3 +1,5 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package de.bytefish.pgbulkinsert.function;
 
 @FunctionalInterface
--- PgBulkInsert/pgbulkinsert-core/src/main/java/de/bytefish/pgbulkinsert/pgsql/model/geometric/Box.java
+++ PgBulkInsert/pgbulkinsert-core/src/main/java/de/bytefish/pgbulkinsert/pgsql/model/geometric/Box.java
@@ -1,4 +1,3 @@
-// Copyright (c) Philipp Wagner. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package de.bytefish.pgbulkinsert.pgsql.model.geometric;
