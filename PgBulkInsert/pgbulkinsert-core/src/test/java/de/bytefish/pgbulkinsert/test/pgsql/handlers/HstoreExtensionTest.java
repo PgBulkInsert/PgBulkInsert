c15f3bcd96a6b21afa3533f828651372baf0cc57
jonfreedman <jon.freedman@zoho.com>
2020-05-19 08:54:27 +0100

    install extension as part of test

--- PgBulkInsert/pgbulkinsert-core/src/test/java/de/bytefish/pgbulkinsert/test/pgsql/handlers/HstoreExtensionTest.java
+++ PgBulkInsert/pgbulkinsert-core/src/test/java/de/bytefish/pgbulkinsert/test/pgsql/handlers/HstoreExtensionTest.java
@@ -7,7 +7,6 @@ import de.bytefish.pgbulkinsert.mapping.AbstractMapping;
 import de.bytefish.pgbulkinsert.test.utils.TransactionalTestBase;
 import de.bytefish.pgbulkinsert.util.PostgreSqlUtils;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.ResultSet;
@@ -35,11 +34,12 @@ public class HstoreExtensionTest extends TransactionalTestBase {
 
     @Override
     protected void onSetUpInTransaction() throws Exception {
+        installExtension();
         createTable();
     }
 
     @Override
-    protected void onSetUpBeforeTransaction() throws Exception {
+    protected void onSetUpBeforeTransaction() {
 
     }
 
@@ -50,6 +50,14 @@ public class HstoreExtensionTest extends TransactionalTestBase {
 
             mapHstore("col_hstore", HStoreEntity::getCol_hstore);
         }
+    }
+
+    private boolean installExtension() throws SQLException {
+        String sqlStatement = "CREATE EXTENSION IF NOT EXISTS hstore;";
+
+        Statement statement = connection.createStatement();
+
+        return statement.execute(sqlStatement);
     }
 
     private boolean createTable() throws SQLException {
@@ -63,7 +71,6 @@ public class HstoreExtensionTest extends TransactionalTestBase {
     }
 
     @Test
-    @Ignore("This Test Requires the hstore extension to be enabled.")
     @SuppressWarnings("unchecked")
     public void saveAll_Hstore_Test() throws SQLException {
 
@@ -93,7 +100,7 @@ public class HstoreExtensionTest extends TransactionalTestBase {
             Map<String, String> v = (Map<String,String>) rs.getObject("col_hstore");
 
             Assert.assertEquals(1, v.size());
-            Assert.assertEquals(true, v.containsKey("Philipp"));
+            Assert.assertTrue(v.containsKey("Philipp"));
             Assert.assertEquals("Cool Cool Cool!", v.get("Philipp"));
         }
     }
